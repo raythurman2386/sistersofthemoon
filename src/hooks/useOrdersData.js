d03624db63fcb8405f2c838a87111d30705cf43b
Raymond Thurman <raymondthurman5@gmail.com>
2021-01-24 21:56:33 -0500

    query hook to pull all orders

---
+++ src/hooks/useOrdersData.js
@@ -0,0 +1,45 @@
+import { useStaticQuery, graphql } from "gatsby"
+
+export const useOrdersData = () => {
+  const { allStripeBalanceTransaction } = useStaticQuery(
+    graphql`
+      query AllOrders {
+        allStripeBalanceTransaction {
+          edges {
+            node {
+              id
+              amount
+              created
+              description
+              status
+              source {
+                amount
+                amount_captured
+                description
+                id
+                paid
+                payment_method
+                receipt_url
+                refunded
+                status
+                customer {
+                  id
+                  email
+                }
+                billing_details {
+                  name
+                  email
+                  address {
+                    country
+                    postal_code
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  return allStripeBalanceTransaction
+}
